1e80f2f76e558d7da82ea0a7699d2b5095f7ab43
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-06-06 23:15:16 +0500

    practice

--- 02LES/index.js
+++ 02LES/index.js
@@ -4,3 +4,8 @@ fs.readFile("./files/starter.txt", "utf-8", (err, data) => {
   if (err) throw err;
   console.log(data);
 });
+
+fs.readFile("./files/starter.txt", "utf-8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
