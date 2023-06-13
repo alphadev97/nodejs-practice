3c1c5ea038133f7c959d2b7cbe9be8f16c9b664c
Muhammad Usama <muhammadusama30289@gmail.com>
2023-06-13 07:12:59 +0500

    update 10LES

--- 10LES/controllers/authController.js
+++ 10LES/controllers/authController.js
@@ -4,3 +4,27 @@ const usersDB = {
     this.users = data;
   },
 };
+const bcrypt = require("bcrypt");
+
+const handleLogin = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res.status(400).json({ message: "Username and Password required" });
+
+  const foundUser = usersDB.users.find((person) => person.username === user);
+
+  if (!foundUser) return res.sendStatus(401); //Unauthorized
+
+  // Evaluate Password
+
+  const match = await bcrypt.compare(pwd, foundUser.password);
+
+  if (match) {
+    // create JWTs
+    res.json({ success: `User ${user} is logged in!` });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = { handleLogin };
