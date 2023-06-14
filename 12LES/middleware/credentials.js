59456149cfdc884c2ab2c3b280cb2c67aba26883
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-06-14 22:32:27 +0500

    updated User roles | authorization

--- 12LES/middleware/credentials.js
+++ 12LES/middleware/credentials.js
@@ -1,10 +1,11 @@
-const allowedOrigins = require("../config/allowedOrigins");
+const allowedOrigins = require('../config/allowedOrigins');
 
 const credentials = (req, res, next) => {
-  const origin = req.header.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Credentials", true);
-  }
-};
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Credentials', true);
+    }
+    next();
+}
 
-module.exports = credentials;
+module.exports = credentials
