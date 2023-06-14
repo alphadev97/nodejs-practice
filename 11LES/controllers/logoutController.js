bd153a42dbbc96c210bdbc2dce116369e5271845
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-06-14 21:10:10 +0500

    updated JWT

--- 11LES/controllers/logoutController.js
+++ 11LES/controllers/logoutController.js
@@ -8,7 +8,7 @@ const usersDB = {
 const fsPromises = require("fs").promises;
 const path = require("path");
 
-const handleLogout = (req, res) => {
+const handleLogout = async (req, res) => {
   // On client, also delete the accessToken
 
   const cookies = req.cookies;
@@ -21,7 +21,7 @@ const handleLogout = (req, res) => {
   );
 
   if (!foundUser) {
-    res.clearCookie("jwt", { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+    res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
     return res.sendStatus(204);
   }
 
@@ -33,11 +33,16 @@ const handleLogout = (req, res) => {
   const currentUser = { ...foundUser, refreshToken: "" };
   usersDB.setUsers([...otherUsers, currentUser]);
   await fsPromises.writeFile(
-    path.join(__dirname, '..', 'model', 'users.json'),
+    path.join(__dirname, "..", "model", "users.json"),
     JSON.stringify(usersDB.users)
   );
 
-  res.clearCookie('jwt', {httpOnly: true, maxAge: 24 * 60 * 60 * 1000}) // secure: true - only serves on https
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    sameSite: "None",
+    secure: true,
+  }); // secure: true - only serves on https
+  res.sendStatus(204);
 };
 
-module.exports = { handleRefreshToken };
+module.exports = { handleLogout };
