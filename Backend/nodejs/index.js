91bb437c5da8603d89567970a16cf01a73bb04e6
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-06-16 01:53:30 +0500

    Node

---
+++ Backend/nodejs/index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/about") {
+    res.end("<h1>About Page</h1>");
+  } else if (req.url === "/") {
+    res.end("<h1>Home Page</h1>");
+  } else if (req.url === "/contact") {
+    res.end("<h1>Contact Page</h1>");
+  } else {
+    res.end("<h1>Page Not Found</h1>");
+  }
+});
+
+server.listen(5000, () => {
+  console.log("Server is working");
+});
