d558802314524978d53d2b2e53696ff4c187e719
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-06-27 01:35:48 +0500

    route splitting

--- Backend/route splitting/app.js
+++ Backend/route splitting/app.js
@@ -1,25 +1,16 @@
 import express from "express";
-import mongoose from "mongoose";
 import userRouter from "./routes/user.js";
+import { connectDB } from "./data/database.js";
 
-const app = express();
+export const app = express();
+
+connectDB();
 
 // Using Middleware
 app.use(express.json());
 
 app.use("/users", userRouter);
 
-mongoose
-  .connect("mongodb://localhost:27017", {
-    dbName: "backendapi",
-  })
-  .then(() => console.log("Database Connected"))
-  .catch((e) => console.log(e));
-
 app.get("/", (req, res) => {
   res.send("Nice Work");
-});
-
-app.listen(4000, () => {
-  console.log("Server is working on port 4000");
 });
