427c634e92e2a611f8946f5dfbce3ea29e1d2dc9
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-06-27 21:50:53 +0500

    todo

---
+++ Backend/todo/routes/user.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  getAllUsers,
+  getUserDetails,
+  login,
+  register,
+} from "../controllers/user.js";
+
+const router = express.Router();
+
+router.get("/all", getAllUsers);
+
+router.post("/new", register);
+
+router.post("/login", login);
+
+router.route("/userid/:id").get(getUserDetails);
+
+export default router;
