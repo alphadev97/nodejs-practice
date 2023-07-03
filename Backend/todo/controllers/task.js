9afd13830ab00d80a18a376ba8f9ad7ad0d93297
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-07-03 19:01:45 +0500

    todo

--- Backend/todo/controllers/task.js
+++ Backend/todo/controllers/task.js
@@ -3,11 +3,15 @@ import { Task } from "../models/task.js";
 export const newTask = async (req, res, next) => {
   const { title, description } = req.body;
 
+  console.log("Working 1");
+
   await Task.create({
     title,
     description,
     user: req.user,
   });
+
+  console.log("Working 2");
 
   res.status(201).json({
     success: true,
